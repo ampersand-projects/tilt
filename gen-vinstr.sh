eaa2594e2fd05def81c949185809a2f9f6b2b528
Song Huang <xiasongh@gmail.com>
2023-06-12 14:53:31 -0400

    create vinstr_str.h at build time instead of configure time

---
+++ gen-vinstr.sh
@@ -0,0 +1,18 @@
+CMAKE_SOURCE_DIR=$1
+CMAKE_CURRENT_BINARY_DIR=$2
+
+clang++ -emit-llvm -S ${CMAKE_SOURCE_DIR}/src/pass/codegen/vinstr.cpp \
+                       -I ${CMAKE_SOURCE_DIR}/include/ \
+                       -o ${CMAKE_CURRENT_BINARY_DIR}/vinstr.ll
+
+VINSTR_IR=$(cat ${CMAKE_CURRENT_BINARY_DIR}/vinstr.ll)
+
+echo "#ifndef INCLUDE_VINSTR_STR_H_
+#define INCLUDE_VINSTR_STR_H_
+static const char* vinstr_str = R\"(
+${VINSTR_IR}
+)\";
+#endif // INCLUDE_VINSTR_STR_H_
+" > ${CMAKE_SOURCE_DIR}/include/tilt/pass/codegen/vinstr_str.h
+
+rm ${CMAKE_CURRENT_BINARY_DIR}/vinstr.ll
