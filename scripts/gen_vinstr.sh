27b3c5264b4db883aca85107c34537af3ef634bf
Anand J <anandj@cs.toronto.edu>
2023-06-13 17:04:26 -0400

    Link vinstr_str.cpp on cmake build

--- scripts/gen_vinstr.sh
+++ scripts/gen_vinstr.sh
@@ -1,7 +1,8 @@
-CMAKE_SOURCE_DIR=$1
-CMAKE_CURRENT_BINARY_DIR=$2
+CMAKE_CXX_COMPILER=$1
+CMAKE_SOURCE_DIR=$2
+CMAKE_CURRENT_BINARY_DIR=$3
 
-clang++ -emit-llvm -S ${CMAKE_SOURCE_DIR}/src/pass/codegen/vinstr.cpp \
+${CMAKE_CXX_COMPILER} -emit-llvm -S ${CMAKE_SOURCE_DIR}/src/pass/codegen/vinstr.cpp \
                    -I ${CMAKE_SOURCE_DIR}/include/ \
                    -o ${CMAKE_CURRENT_BINARY_DIR}/vinstr.ll
 
@@ -10,6 +11,4 @@ VINSTR_IR=$(cat ${CMAKE_CURRENT_BINARY_DIR}/vinstr.ll)
 echo "const char* vinstr_str = R\"(
 ${VINSTR_IR}
 )\";
-" > ${CMAKE_SOURCE_DIR}/src/pass/codegen/vinstr_str.cpp
-
-rm ${CMAKE_CURRENT_BINARY_DIR}/vinstr.ll
+" > ${CMAKE_CURRENT_BINARY_DIR}/vinstr_str.cpp
