09fc96d0323675ac5b3f955aa0879fda45f3d67a
jasperrxzhu <zhu.jasper.98@gmail.com>
2023-06-22 13:25:55 -0400

    Restructured directory, edited CMakeLists.txt and setup.py

--- setup.py
+++ setup.py
@@ -5,8 +5,13 @@ import sys
 from setuptools import Extension, setup
 from setuptools.command.build_ext import build_ext
 
-# extend the Extension class to support CMake build
+# LLVM_INSTALL_PATH environment variable must be set before building pytilt
+LLVM_INSTALL_PATH = os.getenv('LLVM_INSTALL_PATH')
+if(LLVM_INSTALL_PATH == None):
+    sys.exit("Error: LLVM_INSTALL_PATH environment variable not defined.")
+LLVM_DIR = os.getenv('LLVM_INSTALL_PATH') + '/lib/cmake/llvm'
 
+# extend the Extension class to support CMake build
 class CMakeExtension(Extension):
     def __init__(self, name):
         super().__init__(name, sources=[])
@@ -21,33 +26,25 @@ class CMakeBuildExtension(build_ext):
 
         for ext in self.extensions:
             self.build_cmake_ext(ext)
-        # super().run()
 
     def build_cmake_ext(self, ext):
-
         if not os.path.exists(self.build_temp):
             os.makedirs(self.build_temp)
 
         extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
 
-        # manually change LLVM_DIR and TiLT_DIR for the time-being
-        LLVM_DIR = "/home/jasperzhu/llvm-project-11.0.0/build/lib/cmake/llvm"
-        TILT_DIR = "/home/jasperzhu/tilt"
-
         cmake_args = [
             '-DLLVM_DIR={}'.format(LLVM_DIR),
-            '-DTILT_DIRECTORY={}'.format(TILT_DIR),
+            '-DBUILD_PYTILT=ON',
             '-DCMAKE_LIBRARY_OUTPUT_DIRECTORY={}'.format(extdir),
             '-DCMAKE_ARCHIVE_OUTPUT_DIRECTORY={}'.format(self.build_temp),
             '-DPYTHON_EXECUTABLE={}'.format(sys.executable)
         ]
 
-        # ToDo: replace ../..
-        subprocess.check_call( ['cmake'] + cmake_args + ["../.."],
-                               cwd = self.build_temp )
-        subprocess.check_call( ['cmake', '--build', '.'],
-                               cwd = self.build_temp )
-
+        subprocess.check_call(['cmake'] + cmake_args + ["../.."],
+                              cwd = self.build_temp)
+        subprocess.check_call(['cmake', '--build', '.'],
+                              cwd = self.build_temp)
 
 # setup extension module
 setup(
